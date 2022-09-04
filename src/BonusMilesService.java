24d18fc6ace813db3da2b954b102754554f16c90
Елена Кливер <volandmart@mail.ru>
2022-09-04 18:48:27 +0500

    variable edit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-    public int calculate (int cost) {
+    public int calculate(int cost) {
         int result;
-        int x = 20;
-        result = cost / x;
+        int stepOfRubles = 20;
+        result = cost / stepOfRubles;
         return result;
     }
 }
