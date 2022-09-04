f867746b2222a5fa1cf20d32fe41b09ee9c2ff4c
Елена Кливер <volandmart@mail.ru>
2022-09-04 12:28:29 +0500

    Pravka Calculated

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-    public int cost (int a) {
+    public int calculate (int cost) {
         int result;
         int x = 20;
-        result = a / x;
+        result = cost / x;
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 1000;
-        int miles = service.cost(price);
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
